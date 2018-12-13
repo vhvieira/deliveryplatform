34246a636839ebfc97ddcaa28551279ffadeab2c
vhvieira <victor@alphatecti.com.br>
2018-12-13 08:08:16 -0200

    Added alphatec and alphamaq projects

---
+++ Alphatec/Alphamaq_Site/index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>Teste PHP</title>
+ </head>
+ <body>
+<script language="JavaScript"> 
+window.location="https://alphamaq.outsystemscloud.com/external_website/"; 
+</script> 
+<noscript> 
+Se não for direcionado automaticamente, clique <a href="https://alphamaq.outsystemscloud.com/external_website/">aqui</a>. 
+</noscript> 
+ </body>
+</html>
